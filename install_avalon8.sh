616ff49501076320118ade79aec73b8decef0207
firefoxhome <2683299672@qq.com>
2017-11-14 16:57:25 +0800

    avalonminer8 install uninstall scripts

---
+++ install_avalon8.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd ~
+git clone https://github.com/Canaan-Creative/Avalon-extras.git && \
+    cd Avalon-extras && \
+    mkdir -p /home/factory/Desktop && \
+    cp ./scripts/factory/desktop/* /home/factory/Desktop/ && \
+    cd ~/Desktop/ && \
+    rm burnavalon7_* testavalon7_* update_7*  burnauc3.desktop update_auc3* AvalonMiner_test_script.desktop  && \
+    mkdir /home/factory/Xilinx && \
+    mv /home/factory/14.6 Xilinx
+echo "Install finish"
---
+++ uninstall_avalon8.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd ~
+rm -rf Avalon-extras
+rm -rf /home/factory/Desktop/*.desktop
+rm 1.png sgdisk.sh install.sh

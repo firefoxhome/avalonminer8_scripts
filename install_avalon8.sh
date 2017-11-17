5a955487d71923109fee4949874f5662daed03a3
firefoxhome <2683299672@qq.com>
2017-11-17 20:25:19 +0800

    update install_avalon8.sh

--- install_avalon8.sh
+++ install_avalon8.sh
@@ -5,7 +5,7 @@ git clone https://github.com/Canaan-Creative/Avalon-extras.git && \
     mkdir -p /home/factory/Desktop && \
     cp ./scripts/factory/desktop/* /home/factory/Desktop/ && \
     cd ~/Desktop/ && \
-    rm burnavalon7_* testavalon7_* update_7*  burnauc3.desktop update_auc3* AvalonMiner_test_script.desktop  && \
+    rm burnavalon7_* testavalon7_* update_7*  AvalonMiner_test_script.desktop  && \
     mkdir /home/factory/Xilinx && \
     mv /home/factory/14.6 /home/factory/Xilinx
 echo "Install finish"
